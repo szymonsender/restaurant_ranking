bb4d335f01042ac4e44446af1d513bf17a215b13
Szymon Sender <senderszymom@gmail.com>
2022-07-26 19:10:51 +0200

    Implement adding restaurants

--- lib/app/home/add_opinion/add_opinion_page.dart
+++ lib/app/home/add_opinion/add_opinion_page.dart
@@ -1,15 +1,73 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
-class AddOpinionPage extends StatelessWidget {
+class AddOpinionPage extends StatefulWidget {
   const AddOpinionPage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AddOpinionPage> createState() => _AddOpinionPageState();
+}
+
+class _AddOpinionPageState extends State<AddOpinionPage> {
+  var restaurantName = '';
+  var cityName = '';
+  var rating = 2.5;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(''),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę restauracji',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                restaurantName = newValue;
+              });
+            },
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              hintText: 'Podaj nazwę miasta',
+            ),
+            onChanged: (newValue) {
+              setState(() {
+                cityName = newValue;
+              });
+            },
+          ),
+          Slider(
+            onChanged: (newValue) {
+              setState(() {
+                rating = newValue;
+              });
+            },
+            value: rating,
+            min: 0.0,
+            max: 5.0,
+            divisions: 10,
+            label: rating.toString(),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('restaurants').add({
+                'name': restaurantName,
+                'city': cityName,
+                'rating': rating,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }
